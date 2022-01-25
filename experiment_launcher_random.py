57432230a1fd6f114f0533391c0676ebe4af18d9
Andisheh Amrollahi <amrollaa@eu-login-17.euler.ethz.ch>
2022-01-25 20:32:36 +0100

    print

--- experiment_launcher_random.py
+++ experiment_launcher_random.py
@@ -11,6 +11,6 @@ for n in [4, 8, 16, 32, 64, 128, 256]:
                 path = Path(f"results/n={n}_m={m}_d={degree}_{try_no}.json")
                 # if True:
                 if not path.is_file():
-                    os.system(f"bsub -W 23:59 -o logs/log_n={n}_m={m}_d={degree}_{try_no}.out -R rusage[mem=10000] python -u scip_solver.py {n} {m} {degree} {try_no}  &> /dev/null")
-                    print(f"bsub -W 23:59 -o logs/log_n={n}_m={m}_d={degree}_{try_no}.out -R rusage[mem=10000] python -u scip_solver.py {n} {m} {degree} {try_no} &> /dev/null")
+                    os.system(f"bsub -W 3:59 -o logs/log_n={n}_m={m}_d={degree}_{try_no}.out -R rusage[mem=10000] python -u scip_solver.py {n} {m} {degree} {try_no}  &> /dev/null")
+                    print(f"bsub -W 3:59 -o logs/log_n={n}_m={m}_d={degree}_{try_no}.out -R rusage[mem=10000] python -u scip_solver.py {n} {m} {degree} {try_no} &> /dev/null")
 
